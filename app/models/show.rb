3d18498f37086b433acf8c361db722481d43954e
Philip Levy <philipdlevy@gmail.com>
2022-06-12 12:41:48 -0600

    commit tree d5e95ad772cda446144a7d46467d93fdd7cfb3fe

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(show)
+    self.network
+  end
+
+  def actors_list
+    self.actors.map do |name| 
+        "#{name.first_name} #{name.last_name}"
+    end
+  end
 end
